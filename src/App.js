d136504adcc52e09a8c24b9854320b338fdf2698
Precious <Preshomondi@gmail.com>
2022-11-14 13:54:17 +0300

    create states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
 import './App.css';
-
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import React, {useState, useEffect} from "react"
 function App() {
+  const colors = [
+    "114B5F",
+    "1A936F",
+    "420039",
+    "D72638",
+    "A36D90",
+    "EC7D10",
+    "23967F",
+    "3A3E3B",
+    "14342B",
+    "83781B"
+  ];
+  const [background, setBackground] = useState("#A36D90")
+  const [current, setCurrent] = useState(null)
   return (
-    <div className="App">
-      
+    <div className="App" style={{background: background}}>
+      <h1>Color Palette</h1>      
     </div>
   );
 }
